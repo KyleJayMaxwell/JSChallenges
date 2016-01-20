1445175367287d4fcd4146f11169274cbd28db7a
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-01-20 16:50:00 -0700

    Completed Functions-00

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,12 +1,45 @@
 module.exports = {
 
-  helloWorld: function(){
+  helloWorld: function(string){
+    return "Hello World";
   },
 
   hello: function(string){
+    return "Hello "+ string;
   },
 
-  shout: function() {
+  shout: function(string) {
+    var upper = string.toUpperCase();
+    return "Hello "+ upper;
+  },
 
+  whisper: function(string){
+    var lower = string.toLowerCase();
+    return "Hello "+lower;
+  },
+
+  separate: function (string){
+    var split = string.split("");
+    return split;
+  },
+
+  reverseJoin: function (string){
+    var reverse = string.reverse();
+    var join = reverse.join();
+    return join;
+  },
+
+  reverseJoinCompact: function (string){
+    var reverse = string.reverse();
+    var join = reverse.join("");
+    return join;
+  },
+
+  backwardsDay: function (string){
+    var split = string.split("");
+    var reverse = split.reverse();
+    var join = reverse.join("");
+    return "Hello "+ join;
   }
+
 }
