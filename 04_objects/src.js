42207d391ecab75fc7c1c324b7bddb9a3330a183
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-01-25 20:07:59 -0700

    Objects completed

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,34 @@
 module.exports = {
   person: function() {
-    // your code here
+
+    var person = {
+    firstName: "John",
+    lastName: "Mayer",
+    };
+    return person;
+  },
+
+  sweetnessProperty: function(obj){
+    return obj.sweetness;
+  },
+
+  savorynessProperty: function(obj){
+    return obj["savory-ness"];
+  },
+
+  keys: function(obj){
+    return Object.keys(obj);
+  },
+
+  sortedKeys: function(obj){
+    var keys = Object.keys(obj);
+    return keys.sort();
+  },
+
+  reverseSortedKeys: function(obj){
+    var keys = Object.keys(obj);
+    keys.sort();
+    return keys.reverse();
   }
-}
+
+};
